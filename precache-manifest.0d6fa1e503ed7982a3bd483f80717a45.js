c61abb40a820174a024688316f4821fe61e064a6
Shivani <shivanishinde02@gmail.com>
2020-02-12 04:33:18 +0530

    Updates

---
+++ precache-manifest.0d6fa1e503ed7982a3bd483f80717a45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c4d97a8932888f4668d630d19942e0",
+    "url": "/shanu10/index.html"
+  },
+  {
+    "revision": "ebc3010a4d8c327a9717",
+    "url": "/shanu10/static/css/main.8fa3e544.chunk.css"
+  },
+  {
+    "revision": "a63561e3e0b0f2faf86e",
+    "url": "/shanu10/static/js/2.3f5bc241.chunk.js"
+  },
+  {
+    "revision": "fab1da7d8a32be3fe90e332131eacf28",
+    "url": "/shanu10/static/js/2.3f5bc241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc3010a4d8c327a9717",
+    "url": "/shanu10/static/js/main.86ae5784.chunk.js"
+  },
+  {
+    "revision": "a6cf83fbb3c363b5fad7",
+    "url": "/shanu10/static/js/runtime-main.4fda5327.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shanu10/static/media/logo.5d5d9eef.svg"
+  }
+]);
